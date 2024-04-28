456419fdf011e2bda91731b01b8d19fe843e3658
Seshagiri Nuthalapati <nasgrao@gmail.com>
2024-04-27 20:18:55 -0700

    fix(build): learning JS...:)

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -29,8 +29,8 @@ const nextConfig = {
 
 module.exports = (phase, { defaultConfig }) => {
   console.log("----debug---Phase is ... ")
-  console.log($phase)
-  console.log($NEXTAUTH_URL)
+  console.log(phase)
+  console.log(NEXTAUTH_URL)
   // see this https://github.com/vercel/next.js/blob/5e6b008b561caf2710ab7be63320a3d549474a5b/packages/next/shared/lib/constants.ts#L19-L23
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     console.log("Using nextConfigDev")
