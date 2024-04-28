8f7dd167f4d9cb8ce3a8b89f34fcd7f954d46ad0
Seshagiri Nuthalapati <nasgrao@gmail.com>
2024-04-27 20:44:52 -0700

    fix(build): try static block.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -27,7 +27,7 @@ const nextConfig = {
   output: 'standalone'
 }
 
-module.exports = (phase, { defaultConfig }) => {
+/* module.exports = (phase, { defaultConfig }) => {
   console.log("----debug---Phase is ... ")
   console.log(phase)
   console.log(process.env.NEXTAUTH_URL)
@@ -38,4 +38,17 @@ module.exports = (phase, { defaultConfig }) => {
   }
  
   return nextConfig
+} */
+module.exports = {
+  reactStrictMode: true,
+  env: {
+    // pass in NEXTAUTH_URL env. var when deploying to beta/prod
+    NEXTAUTH_SECRET: "gBsuHo9HV6D4zrF+HtLBQ1C8n9W7h37W5beOuDXBw0A=",
+    NEXTAUTH_URL: "https://beta.cellborg.bio"
+
+  },
+  images: {
+    domains: ['images.unsplash.com',]
+  },
+  output: 'standalone'
 }
