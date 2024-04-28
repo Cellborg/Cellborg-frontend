92565111cdbed9bbfc6623013076f9538bfda40a
Seshagiri Nuthalapati <nasgrao@gmail.com>
2024-04-27 20:39:30 -0700

    fix(build): rollback old changes.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -17,8 +17,8 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     // pass in NEXTAUTH_URL env. var when deploying to beta/prod
-    NEXT_PUBLIC_NEXTAUTH_SECRET: "gBsuHo9HV6D4zrF+HtLBQ1C8n9W7h37W5beOuDXBw0A=",
-    NEXT_PUBLIC_NEXTAUTH_URL: "https://beta.cellborg.bio"
+    NEXTAUTH_SECRET: "gBsuHo9HV6D4zrF+HtLBQ1C8n9W7h37W5beOuDXBw0A=",
+    NEXTAUTH_URL: "https://beta.cellborg.bio"
 
   },
   images: {
