146b352dba2e9f76bdab61916b4b775eb1dcf398
Pranik <pranikpant@gmail.com>
2024-04-27 17:51:53 -0700

    update nextauth url

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,7 +5,7 @@ const nextConfigDev = {
   env: {
     // pass in NEXTAUTH_URL env. var when deploying to beta/prod
     NEXTAUTH_SECRET: "gBsuHo9HV6D4zrF+HtLBQ1C8n9W7h37W5beOuDXBw0A=",
-    NEXTAUTH_URL: "http://localhost:3000"
+    NEXTAUTH_URL: "https://beta.cellborg.bio"
 
   },
   images: {
@@ -30,6 +30,7 @@ const nextConfig = {
 module.exports = (phase, { defaultConfig }) => {
   // see this https://github.com/vercel/next.js/blob/5e6b008b561caf2710ab7be63320a3d549474a5b/packages/next/shared/lib/constants.ts#L19-L23
   if (phase === PHASE_DEVELOPMENT_SERVER) {
+    console.log("Using nextConfigDev")
     return nextConfigDev
   }
  
