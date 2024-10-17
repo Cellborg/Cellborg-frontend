d37825a9e5f0e4155b4147130fa92ec6af87ca54
nishant <nishun2005@gmail.com>
2024-10-16 22:26:27 -0700

    fixed wrong callback url. Changed it to be based on env

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,54 +1,43 @@
 /** @type {import('next').NextConfig} */
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+
+const ENV = process.env.NEXT_PUBLIC_DEPLOY_ENV || "dev"// Ensure this points to the correct path
+
 const nextConfigDev = {
   reactStrictMode: true,
   env: {
-    // pass in NEXTAUTH_URL env. var when deploying to beta/prod
     NEXTAUTH_SECRET: "gBsuHo9HV6D4zrF+HtLBQ1C8n9W7h37W5beOuDXBw0A=",
     NEXTAUTH_URL: "http://localhost:3000"
-
   },
   images: {
-    domains: ['images.unsplash.com',]
+    domains: ['images.unsplash.com']
   },
   output: 'standalone'
-}
-const nextConfig = {
+};
+
+const nextConfigProd = {
   reactStrictMode: true,
   env: {
-    // pass in NEXTAUTH_URL env. var when deploying to beta/prod
     NEXTAUTH_SECRET: "gBsuHo9HV6D4zrF+HtLBQ1C8n9W7h37W5beOuDXBw0A=",
     NEXTAUTH_URL: "https://beta.cellborg.bio"
-
   },
   images: {
-    domains: ['images.unsplash.com',]
+    domains: ['images.unsplash.com']
   },
   output: 'standalone'
-}
+};
 
-/* module.exports = (phase, { defaultConfig }) => {
-  console.log("----debug---Phase is ... ")
-  console.log(phase)
-  console.log(process.env.NEXTAUTH_URL)
-  // see this https://github.com/vercel/next.js/blob/5e6b008b561caf2710ab7be63320a3d549474a5b/packages/next/shared/lib/constants.ts#L19-L23
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    console.log("Using nextConfigDev")
-    return nextConfigDev
+// Debug logging for environment check
+console.log("Environment Variable ENVV is:", ENV);
+
+const getNextConfig = () => {
+  if (ENV === 'dev') {
+    console.log("Using Development Config");
+    return nextConfigDev;
+  } else {
+    console.log("Using Production Config");
+    return nextConfigProd;
   }
- 
-  return nextConfig
-} */
-module.exports = {
-  reactStrictMode: true,
-  env: {
-    // pass in NEXTAUTH_URL env. var when deploying to beta/prod
-    NEXTAUTH_SECRET: "gBsuHo9HV6D4zrF+HtLBQ1C8n9W7h37W5beOuDXBw0A=",
-    NEXTAUTH_URL: "http://localhost:3000"
+};
 
-  },
-  images: {
-    domains: ['images.unsplash.com',]
-  },
-  output: 'standalone'
-}
+module.exports = getNextConfig();
