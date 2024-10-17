3337bacab729d396a531edc978e27a16a7a2eba5
nishant <nishun2005@gmail.com>
2024-10-17 14:09:56 -0700

    possible fix again

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 
-const ENV = process.env.NEXT_PUBLIC_DEPLOY_ENV || "dev"// Ensure this points to the correct path
-
 const nextConfigDev = {
   reactStrictMode: true,
   env: {
@@ -47,7 +45,7 @@ module.exports = {
   env: {
     // pass in NEXTAUTH_URL env. var when deploying to beta/prod
     NEXTAUTH_SECRET: "gBsuHo9HV6D4zrF+HtLBQ1C8n9W7h37W5beOuDXBw0A=",
-    NEXTAUTH_URL: "https://beta.cellborg.bio"
+    NEXTAUTH_URL: "http://localhost:3000"
 
   },
   images: {
